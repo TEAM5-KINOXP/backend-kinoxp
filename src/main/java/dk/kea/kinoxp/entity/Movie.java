3bc822475ddbe46db093b7925136df5e1a297ae8
steffengroenandersen <steffengroenandersen@gmail.com>
2023-10-02 11:41:45 +0200

    Add User Entity T5K-5

---
+++ src/main/java/dk/kea/kinoxp/entity/Movie.java
@@ -0,0 +1,31 @@
+package dk.kea.kinoxp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Movie {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String genre;
+    private String posterImg;
+    @CreationTimestamp
+    private LocalDateTime created;
+    @UpdateTimestamp
+    private LocalDateTime edited;
+}
