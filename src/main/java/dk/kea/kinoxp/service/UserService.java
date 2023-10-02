97da4bacbf9f8a8b8f8e9e9eb3d4dfd1eae94f0a
Gi5lund <mortengislund@gmail.com>
2023-10-02 20:36:28 +0200

    basic crud

--- src/main/java/dk/kea/kinoxp/service/UserService.java
+++ src/main/java/dk/kea/kinoxp/service/UserService.java
@@ -3,6 +3,7 @@ package dk.kea.kinoxp.service;
 import dk.kea.kinoxp.entity.UserEntity;
 import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -24,3 +25,26 @@ public class UserService
 			UserEntity user= getUser(username);
 			return new UserResponse(user);
 		}
+public ResponseEntity<Boolean> updateUser(String username, UserRequest userRequest){
+	UserEntity user= getUser(username);
+	if(!userRequest.getUserName().equals(username)){
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Cannot change username");
+		}
+	user.setEmail(userRequest.getEmail());
+	user.setPassword(userRequest.getPassword());
+	user.setFirstName(userRequest.getFirstName());
+	user.setLastName(userRequest.getLastName());
+	userRepository.save(user);
+	return ResponseEntity.ok(true);
+}
+public UserResponse createUser(UserRequest userRequest){
+	UserEntity user=UserRequest.getUserEntity(userRequest);
+	userRepository.save(user);
+	return new UserResponse(user);
+}
+public UserEntity getUser(String username){
+	return userRepository.findById(username).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found"));
+}
+public void deleteUser(String username){
+	userRepository.deleteById(username);
+}
