bb6eea7b2330f4826efb35ad15fc8e6b00dd231c
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-10-02 21:15:23 +0200

    Added basic reservations controller with api adress and dependency inversion

---
+++ src/main/java/dk/kea/kinoxp/api/ReservationController.java
@@ -0,0 +1,16 @@
+package dk.kea.kinoxp.api;
+import dk.kea.kinoxp.service.ReservationService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/reservations")
+public class ReservationController {
+    ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+}
