8ad4db156fbc0fb2fa310da562b3c761272b4230
Gi5lund <mortengislund@gmail.com>
2023-10-02 11:03:41 +0200

    T5K-29-UserController: getAll, Create, delete, update, getuserbyId

---
+++ src/main/java/dk/kea/kinoxp/api/UserController.java
@@ -0,0 +1,38 @@
+package dk.kea.kinoxp.api;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/users")
+
+public class UserController	{
+	UserServivce userService;
+
+	public UserController(UserServivce userService) {
+		this.userService = userService;
+	}
+	//ADMIN refers to T5K-37
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@GetMapping("/admin")
+	public List<UserResponse> getAllUsers() {
+		return userService.getUsers();
+	}
+	@GetMapping(path="/{username}")
+	public UserResponse getUserById(@PathVariable String username) throws Exception {
+		return userService.findById(username);
+	}
+	@PutMapping(path="/{username}")
+	public UserResponse updateUser(@PathVariable String username, @RequestBody UserRequest userRequest) {
+		return userService.updateUser(username, userRequest);
+	}
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public UserResponse createUser(@RequestBody UserRequest userRequest) {
+		return userService.createUser(userRequest);
+	}
+	@DeleteMapping(path="/{username}")
+	void deleteUser(@PathVariable String username) {
+		userService.deleteUser(username);
+	}
