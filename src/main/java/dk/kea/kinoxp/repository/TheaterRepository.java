f1872be443fd51cc33c391a68403da3a42da0957
Gi5lund <mortengislund@gmail.com>
2023-10-06 12:11:41 +0200

    T5K-65 ReservationServiceTest WIP

--- src/main/java/dk/kea/kinoxp/repository/TheaterRepository.java
+++ src/main/java/dk/kea/kinoxp/repository/TheaterRepository.java
@@ -3,5 +3,5 @@ package dk.kea.kinoxp.repository;
 import dk.kea.kinoxp.entity.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TheaterRepository extends JpaRepository<Theater, String> {
+public interface TheaterRepository extends JpaRepository<Theater, Integer> {
 }
